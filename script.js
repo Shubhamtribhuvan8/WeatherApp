dfba789af304e86b363faf093bc3722074f9e9ac
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2022-12-09 00:58:26 +0530

    Weather

---
+++ script.js
@@ -0,0 +1,36 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '33e87d7a45msh410235589638e83p1043c7jsn49eac0499ac1',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+const getWeather = (city)=>{
+    cityName.innerHTML=city
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+    .then(response => response.json())
+    .then((response) => {
+
+        console.log(response)
+        temp.innerHTML = response.temp
+        temp2.innerHTML = response.temp
+        feels_like.innerHTML = response.feels_like
+        humidity.innerHTML = response.humidity
+        humidity2.innerHTML = response.humidity
+        min_temp.innerHTML = response.min_temp
+        max_temp.innerHTML = response.max_temp
+        wind_speed.innerHTML = response.wind_speed
+        wind_speed2.innerHTML = response.wind_speed
+        wind_degrees.innerHTML = response.wind_degrees
+        sunrise.innerHTML = response.sunrise
+        sunset.innerHTML = response.sunset
+    })
+    addEventListener
+	.catch(err => console.error(err));
+}
+      submit.addEventListener("click", (e)=>{
+        e.preventDefault();
+        getWeather(city.value)     
+      })
+
+      getWeather("Delhi");
